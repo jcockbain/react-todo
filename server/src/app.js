0c3a040ffe066a273d9038e0326845774d799efc
James Cockbain <james.cockbain@ibm.com>
2019-11-16 17:05:14 +0000

    added .env

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,12 @@
+require("dotenv").config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
 const cors = require("cors");
 const Tasks = require("./Tasks");
+
+const port = process.env.PORT || 5000;
 
 const middleware = require("./middleware");
 
@@ -22,8 +26,8 @@ app.use("/api/v1/", routes);
 
 app.use(middleware.errorHandler.handleErrors);
 
-app.listen(3000, () => {
-  console.log("App listening on port 3000!");
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
 });
 
 module.exports = app;
